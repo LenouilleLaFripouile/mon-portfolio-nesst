99ab089835872a0340cf2cfedba6f629e205baef
LenouilleLaFripouile <161487400+LenouilleLaFripouile@users.noreply.github.com>
2025-03-19 09:58:10 +0100

    burger html css

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger-arrow").addEventListener("click", function () {
+  document.querySelector(".burger").classList.toggle(".burger-is-active");
+  this.classList.toggle(".burger-is-activeive");
+});
