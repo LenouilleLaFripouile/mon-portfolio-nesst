30e0ea61ae1faa80172d42ad408e97d691f4fec4
LenouilleLaFripouile <161487400+LenouilleLaFripouile@users.noreply.github.com>
2025-12-04 16:25:37 +0100

    swiper-v1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,37 @@
+import { Application } from "@splinetool/runtime";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
 document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
   // --- BURGER ---
   const burger = document.querySelector(".burger");
   const burgerArrow = document.querySelector(".burger-arrow");
@@ -29,10 +62,10 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   videoComps.forEach((comp) => observer.observe(comp));
+
+  const canvas = document.getElementById("canvas3d");
+  if (canvas) {
+    const app = new Application(canvas);
+    app.load("https://prod.spline.design/gA9cR0SG1ezF0eta/scene.splinecode");
+  }
 });
-
-import { Application } from "@splinetool/runtime";
-
-const canvas = document.getElementById("canvas3d");
-const app = new Application(canvas);
-app.load("https://prod.spline.design/gA9cR0SG1ezF0eta/scene.splinecode");
